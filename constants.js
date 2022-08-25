7ceee6f22ce6fa915c61edba279fea026d80d6f2
Prakash <om.pcprakash@gmail.com>
2022-08-26 01:14:30 +0530

    Initial setup for sse

---
+++ constants.js
@@ -0,0 +1,12 @@
+export const RDS_API_URL_SELF = 'https://api.realdevsquad.com/users/self';
+export const UPDATE_SSE_EVENTS_TIME = 5000;
+export const SSE_RESPONSE_HEADER = {
+  Connection: 'keep-alive',
+  'Content-Type': 'text/event-stream',
+  'Cache-Control': 'no-cache',
+};
+export const CORS_OPTIONS = {
+  origin: /(\.realdevsquad\.com$)|(localhost)/,
+  credentials: true,
+};
+export const LOCAL_PORT = 5000;
